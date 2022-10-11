610b7c09d1a3c4f26b34cc8859464c6c24ca39aa
Marius <m.pruteanu@aol.com>
2022-10-11 20:23:20 +0100

    Betting problem added
    Betting problem added

---
+++ Betting - easy 1.3/betting.py
@@ -0,0 +1,8 @@
+import decimal
+
+#read the input and convert into a integer
+n = int(input())
+#calculate and print the option one switch-pauyout-ratio
+print('%.10f'%(100/n))
+#calculate and print the option one switch-pauyout-ratio
+print('%.10f'%(100/(100 -n)))
